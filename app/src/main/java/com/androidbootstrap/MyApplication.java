cea6def00e91249d223dc4485f265fa5af6db06c
puhanhui <puhanhui2012@gmail.com>
2016-07-18 10:28:53 +0800

    add multiDex

---
+++ app/src/main/java/com/androidbootstrap/MyApplication.java
@@ -0,0 +1,12 @@
+package com.androidbootstrap;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * @author puhanhui
+ * @version 1.0
+ * @date 2016/7/18
+ * @since 1.0
+ */
+public class MyApplication extends MultiDexApplication {
+}
